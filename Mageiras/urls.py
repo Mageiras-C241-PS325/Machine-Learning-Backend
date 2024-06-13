cf1ceaf6b6b76e1523061acbbad56869e5b7816b
breadworshipper <azhrayashna@gmail.com>
2024-06-13 21:32:07 +0700

    Modified views for base64 input

--- Mageiras/urls.py
+++ Mageiras/urls.py
@@ -20,5 +20,5 @@ from django.urls import path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("ml_api/", views.ping)
+    path("ml_api/", views.predict)
 ]
--- ml_api/views.py
+++ ml_api/views.py
@@ -5,16 +5,21 @@ from django.conf import settings
 from .apps import MlApiConfig
 from django.http import HttpResponse, JsonResponse
 import tensorflow as tf
+import base64
 
 # Create your views here.
 @api_view(["POST"])
 def predict(request):
     model = MlApiConfig.model
-    file = request.data['file']
+    file = request.data['file'] # is in base64 format
+    # Convert to image
+    image = base64.b64decode(file, validate=True)
     # Convert to tensor
-    tensor = tf.image.decode_image(file.read())
+    tensor = tf.image.decode_image(image)
     # Resize the image
     tensor = tf.image.resize(tensor, [640, 640])
+    tensor = tf.expand_dims(tensor, 0)
+
     prediction = model.predict(tensor)
     
     return HttpResponse(prediction['classes'])
